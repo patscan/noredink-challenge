c172b01869fc63d28db93e563fce3d5d86f76546
patscan <patscan@gmail.com>
2016-05-19 15:08:44 -0700

    Naive implementation of returning actual questions

--- question_collector.rb
+++ question_collector.rb
@@ -1,12 +1,31 @@
 require 'CSV'
 require './models/question'
 require './models/question_impression'
-require 'pry'
 
 class QuestionCollector
   def self.generate_list num
+    num = num.to_i
     questions = self.process_questions
     question_impressions = self.process_impressions
+    best_questions = self.choose_best_questions(questions, question_impressions, num.to_i)
+    best_questions.map(&:question_id)
+  end
+
+  def self.choose_best_questions questions, question_impressions, num
+    # Currently, this naively orders by difficulty only without any accounting 
+    # for strand or standard. Ideally, this would implement a round robin sort using those attributes
+    # before grabbing the result set.
+    result_list = []
+    if num > questions.length
+      until questions.length >= num
+        questions = questions + questions
+      end
+    end
+    questions_ordered_by_ease = questions.sort_by {|q| q.difficulty }.reverse
+    questions_ordered_by_ease.take(num)
+    #until result_list.length == num
+    #  result_list << 
+    #end
   end
 
   def self.process_questions
@@ -22,7 +41,6 @@ class QuestionCollector
       )
     end
     questions
-    #questions.each { |q| puts q.inspect } 
   end
 
   def self.process_impressions
@@ -36,6 +54,5 @@ class QuestionCollector
       )
     end
     question_impressions
-    #question_impressions.each { |q| puts q.inspect } 
   end
 end
--- quizmaker.rb
+++ quizmaker.rb
@@ -3,4 +3,5 @@ require './question_collector'
 puts "How many questions would you like in your quiz?"
 num = gets.chomp 
 puts "Great, I'll generate a list of #{num} question IDs, hang on..."
-QuestionCollector.generate_list num
+result = QuestionCollector.generate_list num
+puts result
